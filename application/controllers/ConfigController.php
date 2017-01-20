4c2caee3fd2ffb5c9ec2d81d383d5e8de1c931b5
StreatCodes <matt.r.streatfield@gmail.com>
2017-01-20 16:11:46 +1100

    Initial commit, pretty much works

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -1,10 +1,10 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
-namespace Icinga\Module\Opsgenie\Controllers;
+namespace Icinga\Module\Agentinstaller\Controllers;
 
 use Icinga\Web\Controller;
-use Icinga\Module\Opsgenie\Forms\Config\GeneralConfigForm;
+use Icinga\Module\Agentinstaller\Forms\Config\GeneralConfigForm;
 
 class ConfigController extends Controller
 {
