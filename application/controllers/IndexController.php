39da7ab28a35f4cf1c48f509022cb94e6dfff498
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-05-01 19:18:59 +1000

    Sorry, a huge diff with error checking, logging...
    ... more robust API calls. But still not fully working yet. Each client, for
    now, gets its own configuration package. Next up is to add logic to check
    if the client has been added previously.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -9,9 +9,13 @@ class AgentInstaller_IndexController extends Controller {
 	public function indexAction() {
         $form = $this->view->form = new CreateInstallerForm;
     }
-    
-	protected function config_string($cname, $caddr, $pzone) {
-		/* Icinga2 API object definitions. */
+
+	/*
+	 * Generate valid Icinga2 configuration for an icinga2 client from the bare
+	 * essentials.
+	 */
+	protected function form_string($cname, $caddr, $pzone) {
+		/* Icinga2 cluster object definitions. */
 		$s  = sprintf("object Endpoint \"%s\" {} ", $cname);
 		$s .= sprintf("object Zone \"%s\" { ", $cname);
 		$s .= sprintf("endpoints = [ \"%s\" ], ", $cname);
@@ -24,35 +28,313 @@ class AgentInstaller_IndexController extends Controller {
 		$t .= sprintf("vars.os = \"%s\", ", "windows");
 		$t .= sprintf("vars.client_endpoint = %s}", "name");
 
-		/*
-		 * Concatentate definitions, returning valid Icinga2 config as one
-		 * fat string.
-		 */
 		$confstr = $s.$t;
-
+		syslog(LOG_DEBUG, "String $confstr generated from web form");
 		return $confstr;
 	}
 
-	protected function config_json($confstr) {
-		/*
-		 * Create an array mapping a client's configuration file to an Icinga2
-		 * configuration string. Pass the generated config array to an array of config
-		 * arrays. Finally return encoded json.
-		 */
-		$a = array("zones.d/$parent_zone/$client_name.conf" => $confstr);
-		$b = array("files" =>  $b);
+	protected function newpackage($package) {
+		$API_username = $this->Config()->get(
+		    'agentinstaller', 'apikey', '');
+		$API_password = $this->Config()->get(
+		    'agentinstaller', 'apipassword', '');
+		$API_url = $this->Config()->get(
+		    'agentinstaller', 'apiaddress', '');
 
-		if (json_encode($b)) {
-			return json_encode($b);
+		$API_url .= "/v1/config/packages/$package";
+
+		$ch = curl_init($API_url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_USERPWD,
+		    $API_username . ":" . $API_password);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type:application/json',
+		    'Accept:application/json')
+		);
+
+		$res = curl_exec($ch);
+		if ($res === FALSE) {
+			throw new Exception(curl_error($ch), curl_errno($ch));
+			$status = -1;
+			error_log("API query to $API_url failed");
+			curl_close($ch);
+			return $status;
 		} else {
-			printf("Error: %d - %s", json_last_error(), json_last_error_msg());
+			$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			curl_close($ch);
+
+			if ($status >= 400) {
+				die("Icinga API returned status $status while
+				    creating $package");
+			} else {
+				if ($json = json_decode($res)) {
+					if ($json->results &&
+					    $json->results[0] &&
+					    $json->results[0]->code &&
+					    (int)$json->results[0]->code === 200) {
+						return true;
+					} else {
+						die("Unexpected response from API");
+					}
+				} else {
+					die("Error decoding API response");
+				}
+			}
+		}
+	}
+
+	/* Find the current 'active-stage' name of configuration packages. */
+	protected function activestage($package) {
+		$API_username = $this->Config()->get('agentinstaller',
+		    'apikey', '');
+		$API_password = $this->Config()->get('agentinstaller',
+		    'apipassword', '');
+		$API_url      = $this->Config()->get('agentinstaller',
+		    'apiaddress', '');
+
+		$API_url .= "/v1/config/packages/$package";
+
+		$ch = curl_init($API_url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type:application/json',
+		    'Accept:application/json'));
+
+		$status = 0;
+		$res = curl_exec($ch);
+		if ($res === FALSE) {
+			throw new Exception(curl_error($ch), curl_errno($ch));
+			$status = -1;
+			error_log("API query to $API_url failed");
+			curl_close($ch);
+			return $status;
+		} else {
+			curl_close($ch);
+			/*
+			 * Loop through the 'results' array. Each entry is a dictionary
+			 * representing each configuration package.
+			 */
+			$jres = json_decode($res, true);
+			$pkg = $jres['results'];
+			foreach ($pkg as $k => $v) {
+				return $v['active-stage'];
+			}
+		}
+	}
+
+	/* For a given stage, enumerate files into an array of files. */
+	protected function lsstage($package, $stage) {
+		$API_username = $this->Config()->get('agentinstaller',
+		    'apikey', '');
+		$API_password = $this->Config()->get('agentinstaller',
+		    'apipassword', '');
+		$API_url      = $this->Config()->get('agentinstaller',
+		    'apiaddress', 'https://localhost:5665');
+
+		$API_url .= "/v1/config/stages/$package/$stage";
+		$ch = curl_init($API_url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type:application/json',
+		    'Accept:application/json'));
+
+		$status = 0;
+		$res = curl_exec($ch);
+		if ($res === FALSE) {
+			$status = -1;
+			throw new Exception(curl_error($ch), curl_errno($ch));
+			error_log("Failed API query to $API_url");
+			curl_close($ch);
+			return $status;
+		} else {
+			/*
+			 * Loop through the 'results' array.
+			 * {
+			 *     name: "host.conf"
+			 *     type: "file"
+			 * },
+			 * {
+			 *     ...
+			 * },
+			 */
+			$jres = json_decode($res, true);
+			$body = $jres->results;
+			$files = array();
+			foreach ($body as $k => $v) {
+				if ($v['type'] == "file") {
+					array_push($files, $v['name']);
+				}
+			}
+			return $files;
+		}
+	}
+
+	/* Read contents of given file from the active stage.  */
+	protected function catconf ($f) {
+		$base = "https://localhost:5665/v1/config/files/_api";
+		$API_url = sprintf("%s/%s/%s", $base, activestage(), $f);
+
+		$API_username = $this->Config()->get('agentinstaller',
+		'apikey', '');
+		$API_password = $this->Config()->get('agentinstaller',
+		'apipassword', '');
+		$API_url      = $this->Config()->get('agentinstaller',
+		'apiaddress', 'https://localhost:5665');
+
+		$ch = curl_init($API_url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$status = 0;
+		$res = curl_exec($ch);
+		syslog(LOG_INFO, "Queried Icinga API at $API_url");
+		if ($res === FALSE) {
+			throw new Exception(curl_error($ch), curl_errno($ch));
+			curl_close($ch);
+			error_log("Failed to read file $f from request to $API_url");
+			$status = -1;
+			return $status;
+		} else {
+			return($res);
+		}
+	}
+
+	/* Main routine. */
+	public function generateAction() {
+		$this->indexAction();
+
+		/* Initialise variables from web form only if defined properly. */
+		$client_name = $_GET['client_name'];
+		if (strlen($client_name) <= 1) {
+			die("Unexpected client name length");
+		}
+		$client_address = $_GET['client_address'];
+		if (strlen($client_address) <= 1) {
+			die("Unexpected client address length");
+		}
+		$parent_name = $_GET['parent_name'];
+		if (strlen($parent_name) <= 1) {
+			die("Unexpected parent name length");
+		}
+		$parent_address = $_GET['parent_address'];
+		if (strlen($parent_address) <= 1) {
+			die("Unexpected parent address length");
+		}
+		$parent_zone = $_GET['parent_zone'];
+		if (strlen($parent_zone) <= 1) {
+			die("Unexpected parent zone length");
+		}
+
+		/* 
+		 * From the current 'stage' list the current files. Create a map of
+		 * configuration filenames to their contents respectively.
+		 */
+
+		$package = "agentinstaller.".$client_name;
+
+		if ($this->newpackage($package) != true) {
+			die("Failed to create package for $client_name");
+		}
+
+		if (($stage = $this->activestage($package) ) < 0) {
+			error_log("Failed to find the active stage");
+		}
+
+		if ($files = $this->lsstage($package, $stage) < 0) {
+			error_log("Error listing files of $stage");
+		}
+
+		$conf = array("", "");
+		for ($i = 0; $i <= count($files); $i++) {
+			if ($s = $this->catconf($files[i]) < 0) {
+				error_log("Error reading configuration file $i from stage");
+			}
+			$conf = array($files[i] => $s);
+		}
+
+		/*
+		 * Generate new file name and its contents from user's input to form
+		 * fields. Append the new data to the configuration map.
+		 */
+		$f = sprintf("zones.d/%s/%s.conf", $parent_zone, $client_name);
+		$confstr = $this->form_string($client_name, $client_address, $parent_zone);
+		$conf[$f] = $confstr;
+
+		/* Encode the configuration map into JSON. */
+		//HOW DO THIS?
+
+		/* Send new configuration package to Icinga */
+		$API_url = "${API_address}/v1/config/stages/agentinstaller";
+		try {
+			$ch = curl_init($API_url);
+
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			    'Content-Type:application/json',
+			    'Accept:application/json'));
+
+			$response = curl_exec($ch);
+			if ($response === FALSE) {
+				throw new Exception(curl_error($ch), curl_errno($ch));
+				curl_close($ch);
+			}
+		} catch(Exception $e) {
+			trigger_error(sprintf('curl error: %d: %s',
+			    $e->getCode(), $e->getMessage()), E_USER_ERROR);
+		}
+	}
+
+	/* Functions to generate an installer file. Not called anywhere yet. */
+	private function makensis() {
+		/* Generate client's signed certificates to workspace dir. */
+		$output_dir = "/var/www/icingaclient/";
+		if ($this->config_ssl("{$output_dir}working-dir") != 0) {
+			printf("Error creating signed client certificates\n");
 			exit(1);
 		}
+
+		/* Generate Icinga2 agent config file for the client. */
+		$client_config = $this->config_agent(
+			$client_name,
+			$parent_name, $parent_address, $parent_zone
+		);
+		file_put_contents($output_dir."working-dir/icinga2.conf", $client_config);
+
+		//run setup generator
+		shell_exec(
+			"sudo -u nagios makensis".
+			"-DPARENT_NAME=$parent_name".
+			"-DCLIENT_NAME=$client_name".
+			"${output_dir}working-dir/buildagent.nsis"
+		);
+
+		//cleanup files
+		unlink("{$output_dir}working-dir/{$client_name}.crt");
+		unlink("{$output_dir}working-dir/{$client_name}.csr");
+		unlink("{$output_dir}working-dir/{$client_name}.key");
+
+		//Download link, necessary due to everthing being an XHR request
+		echo "<a href='../download?clientname=${client_name}' target='_blank'>Download installer</a>";
 	}
 
 	protected function config_ssl($outdir) {
 		$safe_client = escapeshellarg($client_name);
-
 		$cert_res = shell_exec("sudo -u nagios icinga2 pki new-cert ".
 			"--cn $safe_client ".
 			"--key {$outdir}/$safe_client.key ".
@@ -88,202 +370,5 @@ class AgentInstaller_IndexController extends Controller {
 
 		return $f;
 	}
-
-	/* Find the current 'active-stage' name of configuration packages. */
-	protected function activestage() {
-		$url = "https://localhost:5665/v1/config/packages";
-
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt(
-			$ch, CURLOPT_HTTPHEADER, array(
-				'Content-Type:application/json',
-				'Accept:application/json'
-			)
-		);
-
-		$res = curl_exec($ch);
-		if ($res === FALSE) {
-				throw new Exception(curl_error($ch), curl_errno($ch));
-				curl_close($ch);
-		} else {
-			curl_close($ch);
-			/*
-			 * Loop through the 'results' array. Each entry is a dictionary
-			 * representing each configuration package.
-			 */
-			$jres = json_decode($res, true);
-			$pkg = $jres['results'];
-			foreach ($pkg as $k => $v) {
-				return $v['active-stage'];
-			}
-		}
-	}
-
-	/* For a given stage, list its files as entries in an array. */
-	protected function lsstage($stage) {
-		$url = "https://suboptic.sol1.net:5665/v1/config/stages/agentinstaller/";
-		$url .= $stage;
-
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type:application/json',
-			'Accept:application/json'
-			)
-		);
-
-		$res = curl_exec($ch);
-		if ($res === FALSE) {
-				throw new Exception(curl_error($ch), curl_errno($ch));
-				curl_close($ch);
-		} else {
-			/*
-			 * Loop through the 'results' array.
-			 * {
-			 *     name: "host.conf"
-			 *     type: "file"
-			 * },
-			 * {
-			 *     ...
-			 * },
-			 */
-			$jres = json_decode($res, true);
-			$body = $jres['results'];
-			$files = array();
-			foreach ($body as $k => $v) {
-				if ($v['type'] == "file") {
-					array_push($files, $v['name']);
-				}
-			}
-			return $files;
-		}
-	}
-
-	/* Read contents of given file from the active stage.  */
-	protected function catconf ($f) {
-		$base = "https://suboptic.sol1.net:5665/v1/config/files/_api";
-		$url = sprintf("%s/%s/%s", $base, activestage(), $f);
-
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$res = curl_exec($ch);
-		if ($res === FALSE) {
-			throw new Exception(curl_error($ch), curl_errno($ch));
-			curl_close($ch);
-		} else {
-			return($res);
-		}
-	}
-
-	// Main routine
-	public function generateAction() {
-		/* Initialise variables from web form only if defined properly. */
-		$client_name = $_GET['client_name'];
-		if strlen($client_name <= 1) {
-			die("Unexpected client name length")
-		}
-		$client_address   = $_GET['client_address'];
-		if strlen($client_address <= 1) {
-			die("Unexpected client address length")
-		}
-		$parent_name = $_GET['parent_name'];
-		if strlen($parent_name <= 1) {
-			die("Unexpected parent name length");
-		}
-		$parent_address = $_GET['parent_address'];
-		if strlen($parent_address <= 1) {
-			die("Unexpected parent address length");
-		}
-		$parent_zone = $_GET['parent_zone'];
-		if strlen($parent_zone <= 1) {
-			die("Unexpected parent zone length");
-		}
-
-
-		// Generate the 'configuration package' api request body.
-		// See 'Configuration Management' in the Icinga2 API documentation for
-		// format. 
-		$confstr = $this->config_string($client_name, $client_address, $parent_zone);
-		if ($confstr < 0) {
-			die("Error creating string from input parameters");
-		}
-
-		$API_username = $this->Config()->get('agentinstaller', 'apikey', 'no username');
-		$API_password = $this->Config()->get('agentinstaller', 'apipassword', 'no password');
-		$API_address = $this->Config()->get('agentinstaller', 'apiaddress', 'https://localhost:5665');
-
-		$url = "${API_address}/v1/config/stages/agentinstaller";
-
-		try {
-			$ch = curl_init($url);
-
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_USERPWD, $API_username . ":" . $API_password);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Content-Type:application/json',
-				'Accept:application/json'
-				)
-			);
-
-			$response = curl_exec($ch);
-			if ($response === FALSE) {
-				throw new Exception(curl_error($ch), curl_errno($ch));
-				curl_close($ch);
-			}
-		} catch(Exception $e) {
-			trigger_error(
-				sprintf('curl error: %d: %s', $e->getCode(), $e->getMessage()),
-				E_USER_ERROR
-			);
-		}
-	}
-
-	private function makensis() {
-		/* Generate client's signed certificates to workspace dir. */
-		$output_dir = "/var/www/icingaclient/";
-		if ($this->config_ssl("{$output_dir}working-dir") != 0) {
-			printf("Error creating signed client certificates\n");
-			exit(1);
-		}
-
-		/* Generate Icinga2 agent config file for the client. */
-		$client_config = $this->config_agent(
-			$client_name,
-			$parent_name, $parent_address, $parent_zone
-		);
-		file_put_contents($output_dir."working-dir/icinga2.conf", $client_config);
-
-		//run setup generator
-		shell_exec(
-			"sudo -u nagios makensis".
-			"-DPARENT_NAME=$parent_name".
-			"-DCLIENT_NAME=$client_name".
-			"${output_dir}working-dir/buildagent.nsis"
-		);
-
-		//cleanup files
-		unlink("{$output_dir}working-dir/{$client_name}.crt");
-		unlink("{$output_dir}working-dir/{$client_name}.csr");
-		unlink("{$output_dir}working-dir/{$client_name}.key");
-
-		//Download link, necessary due to everthing being an XHR request
-		echo "<a href='../download?clientname=${client_name}' target='_blank'>Download installer</a>";
-	}
 }
-
 ?>
